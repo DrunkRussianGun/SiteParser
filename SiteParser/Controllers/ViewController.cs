fc15adce6c15cbdf27c3a70c4d3f99aaa7b11d52
Drunk Russian Gun <drunkrussiangun@gmail.com>
2020-01-27 10:48:01 +0300

    Добавил заготовку проекта

---
+++ SiteParser/Controllers/ViewController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteParser.Controllers
+{
+    public class ViewController : Controller
+    {
+        [HttpGet]
+        public IActionResult Search()
+        {
+            return View();
+        }
+    }
+}
