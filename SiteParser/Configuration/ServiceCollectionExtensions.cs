9a7c7ed9795beeff3afeff3b80e5866e46907a05
Drunk Russian Gun <drunkrussiangun@gmail.com>
2020-01-27 12:08:44 +0300

    Заготовки для реализации индексации

--- SiteParser/Configuration/ServiceCollectionExtensions.cs
+++ SiteParser/Configuration/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MoreLinq;
 using Nest;
 using SiteParser.Implementation;
 using SiteParser.Implementation.Database;
@@ -21,6 +22,17 @@ namespace SiteParser.Configuration
         public static void AddImplementation(this IServiceCollection services)
         {
             services.AddSingleton<Searcher>();
+            services.AddSingleton<Indexer>();
+        }
+
+        public static void AddHttpClient(this IServiceCollection services, IConfiguration configuration, string clientName)
+        {
+            var headers = configuration.GetHttpClientHeaders();
+            services.AddHttpClient(clientName, client =>
+                headers.ForEach(header =>
+                    client.DefaultRequestHeaders.Add(header.Key, header.Value)
+                )
+            );
         }
     }
 }
---
+++ SiteParser/Implementation/Indexer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using SiteParser.Models;
+
+namespace SiteParser.Implementation
+{
+	public class Indexer
+	{
+		public Indexer()
+		{
+			
+		}
+
+		public async Task<IndexingResult> IndexAsync(Uri pageUrl)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
